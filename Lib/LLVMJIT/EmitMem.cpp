d04fa0703db037f83fa8bb010dfccfed08f42203
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-03-18 03:55:53 -0400

    unknown reason

--- Lib/LLVMJIT/EmitMem.cpp
+++ Lib/LLVMJIT/EmitMem.cpp
@@ -162,7 +162,8 @@ static void createmtgconditionaltrap(EmitFunctionContext& functionContext, ::llv
 		irBuilder.CreateCondBr(cmpres, trapBlock, normalBlock);
 		// irBuilder.CreateBr(trapBlock);
 		irBuilder.SetInsertPoint(trapBlock);
-		irBuilder.CreateCall(getWavmMemtagTrapFunction(functionContext));
+		irBuilder.CreateIntrinsic(::llvm::Intrinsic::trap, {}, {});
+		//irBuilder.CreateCall(getWavmMemtagTrapFunction(functionContext));
 		irBuilder.CreateUnreachable();
 		// irBuilder.CreateBr(normalBlock);
 		irBuilder.SetInsertPoint(normalBlock);
