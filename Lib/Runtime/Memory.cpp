715f58e7d2bb56fd4f76271c6f2f501d6e2a454c
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-11-29 21:59:23 -0500

    meme

--- Lib/Runtime/Memory.cpp
+++ Lib/Runtime/Memory.cpp
@@ -387,19 +387,22 @@ GrowResult Runtime::growMemory(Memory* memory, Uptr numPagesToGrow, Uptr* outOld
 			}
 		}
 #ifdef __aarch64__
-		else if(memory->memtagstatus == ::WAVM::LLVMJIT::memtagStatus::armmte)
+		else if (memory->memtagstatus == ::WAVM::LLVMJIT::memtagStatus::armmte)
 		{
-			auto baseAddress{memory->baseAddress};
-			void* taggedAddress;
-			__asm__ __volatile__(
-				R"(
-			.arch_extension memtag
-			irg %[taggedAddress], %[baseAddress], #1
-			stg %[taggedAddress], %[taggedAddress]
-			)"
-				: [taggedAddress] "=r"(taggedAddress)
-				: [baseAddress] "r"(baseAddress)
-				: "memory");
+			uintptr_t baseAddress = reinterpret_cast<uintptr_t>(memory->baseAddress);
+			uintptr_t result;
+
+			__asm__ __volatile__("irg %0, %1, %2"
+			: "=r" (result)
+			: "r" (baseAddress), "r" (0x1)
+			);
+
+			__asm__ __volatile__("stg %0, %1"
+			:
+			: "r" (result), "r" (0x1)
+			: "memory"
+			);
+//			__builtin_arm_stg(__builtin_arm_irg(memory->baseAddress, 0x1));
 		}
 #endif
 		Platform::registerVirtualAllocation(grownpages);
