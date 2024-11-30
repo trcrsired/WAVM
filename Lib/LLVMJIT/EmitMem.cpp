58354bd7dd757d52810f652986d7becc7cab29ee
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-11-29 22:53:28 -0500

    emitmem

--- Lib/LLVMJIT/EmitMem.cpp
+++ Lib/LLVMJIT/EmitMem.cpp
@@ -1359,9 +1359,11 @@ void EmitFunctionContext::memtag_load(MemoryImm imm)
 				memaddress = irBuilder.CreateIntrinsic(
 					::llvm::Intrinsic::aarch64_ldg,
 					{},
-					{memaddress});
+					{memaddress, memaddress});
+#if 0
 				memaddress = armmte64_to_32_old_value(
 					*this, imm.memoryIndex, olduntaggedmemaddress, memaddress);
+#endif
 			}
 		}
 		else
