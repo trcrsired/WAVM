807ba2106f7286539568d0eeb79a33fb18355c71
trcrsired <uwgghhbcad@gmail.com>
2026-02-01 21:08:55 +0800

    Event.h should always include pthread.h or it won't build for loongarch and riscv and many other architectures

--- Include/WAVM/Platform/Event.h
+++ Include/WAVM/Platform/Event.h
@@ -3,6 +3,22 @@
 #include "WAVM/Inline/BasicTypes.h"
 #include "WAVM/Inline/Time.h"
 #include "WAVM/Platform/Defines.h"
+#ifndef _WIN32
+#if defined(__WAVIX__)
+namespace WAVM {
+	inline constexpr size_t wavm_pthread_mutex_words = 6;
+	inline constexpr size_t wavm_pthread_cond_words = 12;
+}
+#else
+#include <pthread.h>
+namespace WAVM {
+	inline constexpr size_t wavm_pthread_mutex_words
+		= (sizeof(pthread_mutex_t) + sizeof(Uptr) - 1) / sizeof(Uptr);
+	inline constexpr size_t wavm_pthread_cond_words
+		= (sizeof(pthread_cond_t) + sizeof(Uptr) - 1) / sizeof(Uptr);
+}
+#endif
+#endif
 
 namespace WAVM { namespace Platform {
 	// Platform-independent events.
@@ -25,54 +41,15 @@ namespace WAVM { namespace Platform {
 	private:
 #ifdef _WIN32
 		void* handle;
-#elif defined(__BIONIC__)
-		struct PthreadMutex
-		{
-			Uptr data[5];
-		} pthreadMutex;
-		struct PthreadCond
-		{
-			Uptr data[6];
-		} pthreadCond;
-
-#elif defined(__linux__) && defined(__x86_64__)
-		struct PthreadMutex
-		{
-			Uptr data[5];
-		} pthreadMutex;
-		struct PthreadCond
-		{
-			Uptr data[6];
-		} pthreadCond;
-#elif defined(__linux__) && defined(__aarch64__)
-		struct PthreadMutex
-		{
-			Uptr data[6];
-		} pthreadMutex;
-		struct PthreadCond
-		{
-			Uptr data[6];
-		} pthreadCond;
-#elif defined(__APPLE__)
-		struct PthreadMutex
-		{
-			Uptr data[8];
-		} pthreadMutex;
-		struct PthreadCond
-		{
-			Uptr data[6];
-		} pthreadCond;
-#elif defined(__WAVIX__)
-		struct PthreadMutex
-		{
-			Uptr data[6];
-		} pthreadMutex;
-		struct PthreadCond
-		{
-			Uptr data[12];
-		} pthreadCond;
 #else
-#error unsupported platform
+		struct PthreadMutex
+		{
+			Uptr data[::WAVM::wavm_pthread_mutex_words];
+		} pthreadMutex;
+		struct PthreadCond
+		{
+			Uptr data[::WAVM::wavm_pthread_cond_words];
+		} pthreadCond;
 #endif
 	};
 }}
